1687add261ef0226e91a31367b39d98ae779ce70
matcher9131 <matcher9131cd@gmail.com>
2024-03-24 09:51:59 +0900

    プロジェクト ファイルを追加します。

---
+++ MultiTimer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace MultiTimer.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "MultiTimer";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
