5d4715e2310ee9d59b1d8d6d1d6a1706b08a528d
matcher9131 <matcher9131cd@gmail.com>
2024-04-02 10:24:55 +0900

    Implement tests: moving Timers up/down test in MainWindowViewModel

--- TestProject/MainWindowViewModelTest.cs
+++ TestProject/MainWindowViewModelTest.cs
@@ -13,9 +13,13 @@ namespace TestProject
 {
     public class MainWindowViewModelTest
     {
-        private (MainWindowViewModel vm, Action<TimerViewModel> removeTimerViewModelAction) CreateViewModel()
+        record CreateViewModelReturnType(MainWindowViewModel ViewModel, Action<TimerViewModel> RemoveTimerAction, Action<TimerViewModel> MoveTimerUpAction, Action<TimerViewModel> MoveTimerDownAction);
+
+        private static CreateViewModelReturnType CreateViewModel()
         {
-            Action<TimerViewModel> removeTimerViewModelAction = _ => { };
+            Action<TimerViewModel> removeTimerAction = _ => { };
+            Action<TimerViewModel> moveTimerUpAction = _ => { };
+            Action<TimerViewModel> moveTimerDownAction = _ => { };
 
             var removeSelfEventMock = new Mock<RemoveSelfEvent>();
             removeSelfEventMock.Setup(x => x.Subscribe(
@@ -23,20 +27,38 @@ namespace TestProject
                 It.IsAny<ThreadOption>(),
                 It.IsAny<bool>(),
                 It.IsAny<Predicate<TimerViewModel>>()
-            )).Callback<Action<TimerViewModel>, ThreadOption, bool, Predicate<TimerViewModel>>((action, _, _, _) => { removeTimerViewModelAction = action; });
+            )).Callback<Action<TimerViewModel>, ThreadOption, bool, Predicate<TimerViewModel>>((action, _, _, _) => { removeTimerAction = action; });
+
+            var moveTimerUpEventMock = new Mock<MoveUpEvent>();
+            moveTimerUpEventMock.Setup(x => x.Subscribe(
+                It.IsAny<Action<TimerViewModel>>(),
+                It.IsAny<ThreadOption>(),
+                It.IsAny<bool>(),
+                It.IsAny<Predicate<TimerViewModel>>()
+            )).Callback<Action<TimerViewModel>, ThreadOption, bool, Predicate<TimerViewModel>>((action, _, _, _) => { moveTimerUpAction = action; });
+
+            var moveTimerDownEventMock = new Mock<MoveDownEvent>();
+            moveTimerDownEventMock.Setup(x => x.Subscribe(
+                It.IsAny<Action<TimerViewModel>>(),
+                It.IsAny<ThreadOption>(),
+                It.IsAny<bool>(),
+                It.IsAny<Predicate<TimerViewModel>>()
+            )).Callback<Action<TimerViewModel>, ThreadOption, bool, Predicate<TimerViewModel>>((action, _, _, _) => { moveTimerDownAction = action; });
 
             var eventAggregatorMock = new Mock<IEventAggregator>();
             eventAggregatorMock.Setup(x => x.GetEvent<RemoveSelfEvent>()).Returns(removeSelfEventMock.Object);
+            eventAggregatorMock.Setup(x => x.GetEvent<MoveUpEvent>()).Returns(moveTimerUpEventMock.Object);
+            eventAggregatorMock.Setup(x => x.GetEvent<MoveDownEvent>()).Returns(moveTimerDownEventMock.Object);
 
             var confirmDialogServiceMock = new Mock<IConfirmDialogService>();
             var vm = new MainWindowViewModel(eventAggregatorMock.Object, confirmDialogServiceMock.Object);
-            return (vm, removeTimerViewModelAction);
+            return new CreateViewModelReturnType(vm, removeTimerAction, moveTimerUpAction, moveTimerDownAction);
         }
 
         [Fact(DisplayName = "AddTimerCommandを実行するとTimers.Countが増える")]
         public void AddTest()
         {
-            var (vm, _) = CreateViewModel();
+            var (vm, _, _, _) = CreateViewModel();
 
             Assert.Equal(1, vm.Timers.Count);
 
@@ -48,7 +70,7 @@ namespace TestProject
         [Fact(DisplayName = "TimerViewModelのRemoveSelfEventを実行するとそのインスタンスがTimersから消去される")]
         public void RemoveTest()
         {
-            var (vm, removeTimerViewModelAction) = CreateViewModel();
+            var (vm, removeTimerViewModelAction, _ ,_) = CreateViewModel();
 
             vm.AddTimerCommand.Execute();
 
@@ -64,37 +86,105 @@ namespace TestProject
         [Fact(DisplayName = "Timers内の先頭以外のTimerViewModelのMoveUpEventが実行されるとそのインスタンスと直前のインスタンスの順番が入れ替わる")]
         public void MoveUpTest()
         {
-            throw new NotImplementedException();
+            var (vm, _, moveTimerUpAction, _) = CreateViewModel();
+
+            vm.AddTimerCommand.Execute();
+            vm.AddTimerCommand.Execute();
+            var timer0 = vm.Timers[0];
+            var timer1 = vm.Timers[1];
+            var timer2 = vm.Timers[2];
+            moveTimerUpAction.Invoke(timer1);
+
+            Assert.Equal(1, vm.Timers.IndexOf(timer0));
+            Assert.Equal(0, vm.Timers.IndexOf(timer1));
+            Assert.Equal(2, vm.Timers.IndexOf(timer2));
         }
 
         [Fact(DisplayName = "Timers内の先頭のTimerViewModelでMoveUpEventが実行されても順番が変化しない")]
         public void MoveUpDisabledTest()
         {
-            throw new NotImplementedException();
+            var (vm, _, moveTimerUpAction, _) = CreateViewModel();
+
+            vm.AddTimerCommand.Execute();
+            vm.AddTimerCommand.Execute();
+            var timer0 = vm.Timers[0];
+            var timer1 = vm.Timers[1];
+            var timer2 = vm.Timers[2];
+            moveTimerUpAction.Invoke(timer0);
+
+            Assert.Equal(0, vm.Timers.IndexOf(timer0));
+            Assert.Equal(1, vm.Timers.IndexOf(timer1));
+            Assert.Equal(2, vm.Timers.IndexOf(timer2));
         }
 
         [Fact(DisplayName = "Timersの要素でないTimerViewModelでMoveUpEventが実行されたときに例外を投げる")]
         public void MoveUpInvalidTest()
         {
-            throw new NotImplementedException();
+            var (vm, removeTimerAction, moveTimerUpAction, _) = CreateViewModel();
+
+            // 空のTimerViewModelを新たに作るのはモックなどが面倒なので、一度追加されて取り除かれたTimerViewModelで代用する
+            vm.AddTimerCommand.Execute();
+            var removedTimerViewModel = vm.Timers[1];
+            removeTimerAction.Invoke(removedTimerViewModel);
+
+            void act()
+            { 
+                moveTimerUpAction.Invoke(removedTimerViewModel);
+            };
+
+            Assert.Throws<ArgumentException>(act);
         }
 
         [Fact(DisplayName = "Timers内の末尾以外のTimerViewModelでMoveDownEventが実行されるとそのインスタンスと直後のインスタンスの順番が入れ替わる")]
         public void MoveDownTest()
         {
-            throw new NotImplementedException();
+            var (vm, _, _, moveTimerDownAction) = CreateViewModel();
+
+            vm.AddTimerCommand.Execute();
+            vm.AddTimerCommand.Execute();
+            var timer0 = vm.Timers[0];
+            var timer1 = vm.Timers[1];
+            var timer2 = vm.Timers[2];
+            moveTimerDownAction.Invoke(timer0);
+
+            Assert.Equal(1, vm.Timers.IndexOf(timer0));
+            Assert.Equal(0, vm.Timers.IndexOf(timer1));
+            Assert.Equal(2, vm.Timers.IndexOf(timer2));
         }
 
         [Fact(DisplayName = "Timers内の末尾のTimerViewModelでMoveDownEventが実行されても順番が変化しない")]
         public void MoveDownDisabledTest()
         {
-            throw new NotImplementedException();
+            var (vm, _, _, moveTimerDownAction) = CreateViewModel();
+
+            vm.AddTimerCommand.Execute();
+            vm.AddTimerCommand.Execute();
+            var timer0 = vm.Timers[0];
+            var timer1 = vm.Timers[1];
+            var timer2 = vm.Timers[2];
+            moveTimerDownAction.Invoke(timer2);
+
+            Assert.Equal(0, vm.Timers.IndexOf(timer0));
+            Assert.Equal(1, vm.Timers.IndexOf(timer1));
+            Assert.Equal(2, vm.Timers.IndexOf(timer2));
         }
 
         [Fact(DisplayName = "Timersの要素でないTimerViewModelでMoveDownEventが実行されたときに例外を投げる")]
         public void MoveDownInvalidTest()
         {
-            throw new NotImplementedException();
+            var (vm, removeTimerAction, _, moveTimerDownAction) = CreateViewModel();
+
+            // 空のTimerViewModelを新たに作るのはモックなどが面倒なので、一度追加されて取り除かれたTimerViewModelで代用する
+            vm.AddTimerCommand.Execute();
+            var removedTimerViewModel = vm.Timers[1];
+            removeTimerAction.Invoke(removedTimerViewModel);
+
+            void act()
+            {
+                moveTimerDownAction.Invoke(removedTimerViewModel);
+            };
+
+            Assert.Throws<ArgumentException>(act);
         }
     }
 }
